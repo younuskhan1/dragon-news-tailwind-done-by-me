874a22e7fdd68ca5bbb607ba97ade2991c9be5b7
younuskhan1 <md.younuskhan412@gmail.com>
2024-03-10 10:47:59 +0600

    active button is added dynamically

--- src/Components/LeftSideNews/LeftSideNews.jsx
+++ src/Components/LeftSideNews/LeftSideNews.jsx
@@ -25,15 +25,26 @@ const LeftSideNews = ({getSelectedCategory}) => {
         fetchData();
     },[]);
 
+        let button_list = document.querySelectorAll(".category-button");
+        button_list.forEach(element=>{
+            element.addEventListener("click",function(){
+            button_list.forEach(remove_background=>{
+            remove_background.style.color="#000000";
+            remove_background.style.background="#ECECEC";
+            });
+            this.style.color="#FFFFFF";
+            this.style.background="#000000";
+        });
+     });
 
     return (
         <div className="w-[100%] lg:w-[22%] mb-5">
             <h1 className="text-center text-black text-xl font-semibold">All Categories</h1>
             <ul className='pt-8'>
                 {categoriesNames.map((categoryName) => 
-                <li className='category-button py-2 px-4 hover:bg-slate-200 rounded-sm text-center 
-                mb-3 cursor-pointer text-[#9f9f9f] hover:text-[#403f3f] font-bold  
-                transition-all ease-in duration-300' key={categoryName.id} onClick={()=>getSelectedCategory(categoryName.id)}>{categoryName.name}</li>)
+                <li className='category-button py-2 px-4 rounded-md text-center
+                mb-3 cursor-pointer font-bold bg-[#ECECEC]' 
+                key={categoryName.id} onClick={()=>getSelectedCategory(categoryName.id)}>{categoryName.name}</li>)
                 }
             </ul>
 
